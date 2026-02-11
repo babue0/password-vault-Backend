468053cd7c2503589b6aac87dc929191221c6785
babue0 <iandpcarvalho@gmail.com>
2026-02-11 20:52:28 -0300

    url

--- src/main/java/com/br/passwordvault/config/WebConfig.java
+++ src/main/java/com/br/passwordvault/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig  implements WebMvcConfigurer {
             .allowedOrigins(
                     "http://localhost:5500",
                     "http://127.0.0.1:5500",
-                    "https://password-vault-pi-gray.vercel.app"
+                    "https://password-vault-pi-gray.vercel.app",
+                    "password-vault-pi-gray.vercel.app"
             )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowCredentials(true);
